c3f33d049f01e7497c5b95fb44e0b885b2967681
gerry-mandering <kimmen1119@gmail.com>
2022-06-23 22:44:08 +0900

    Feat: loop

---
+++ 5.Condition_Repetition/loop.java
@@ -0,0 +1,15 @@
+public class loop
+{
+	public static void main(String[] args)
+	{
+		int	count = 0;
+
+		for (;;)
+		{
+			System.out.println("출력");
+			count++;
+			if (count == 10)
+				break;
+		}
+	}
+}
