f70cc9e916dfb5ca5a0b1c6ea6d3eaec09984402
gerry-mandering <kimmen1119@gmail.com>
2022-06-25 15:23:12 +0900

    Feat: Final

---
+++ 13.Final/Main.java
@@ -0,0 +1,14 @@
+public class Main extends Parent
+{
+	public void show()
+	{
+		System.out.println("Nop!");
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+
+		main.show();
+	}
+}
---
+++ 13.Final/final_variable.java
@@ -0,0 +1,10 @@
+public class final_variable
+{
+	public static void main(String[] args)
+	{
+		final int number = 10;
+
+		System.out.println(number);
+		//number = 30;
+	}
+}
